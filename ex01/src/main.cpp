f1d466d3ca73c57ef7dee9c7c34f6844582ad9c9
Barbara LEFEBVRE <blefebvr@made-f0Dr7s12.clusters.42paris.fr>
2023-10-24 17:05:12 +0200

    add 24-10 ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: blefebvr <blefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 17:14:02 by blefebvr          #+#    #+#             */
-/*   Updated: 2023/10/24 16:55:15 by blefebvr         ###   ########.fr       */
+/*   Updated: 2023/10/24 17:04:30 by blefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,47 @@
 
 int main()
 {
+	std::cout << RED "******* Manual array filling *******" DEFAULT << std::endl;
+	Span sp = Span(5);
 	try
 	{
-		std::cout << RED "******* Manual array filling *******" DEFAULT << std::endl;
-		Span sp = Span(5);
 		sp.addNumber(6);
 		sp.addNumber(3);
 		sp.addNumber(17);
 		sp.addNumber(9);
 		sp.addNumber(11);
-		//sp.addNumber(25);
-		std::cout << sp << std::endl;
+		sp.addNumber(25);
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+	std::cout << sp << std::endl;
 	
+	std::cout << RED "******* Automatic array filling *******" DEFAULT << std::endl;
+	Span sp2 = Span(1000);
 	try
 	{
-		std::cout << RED "******* Automatic array filling*******" DEFAULT << std::endl;
-		Span sp2 = Span(1000);
 		sp2.addMultipleNb();
-		//sp2.addNumber(25);
-		std::cout << sp2 << std::endl;
+		sp2.addNumber(25);
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-
+	std::cout << sp2 << std::endl;
+	
+	std::cout << RED "******* Another exception test *******" DEFAULT << std::endl;
+	Span sp3 = Span(1);
 	try
 	{
-		std::cout << RED "******* Manual array filling *******" DEFAULT << std::endl;
-		Span sp3 = Span(1);
 		sp3.addNumber(6);
-		sp3.addNumber(3);
-		sp3.addNumber(17);
-		sp3.addNumber(9);
-		sp3.addNumber(11);
-		//sp.addNumber(25);
 		std::cout << sp3 << std::endl;
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+	
 	return (0);
 }
